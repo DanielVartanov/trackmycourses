f4b728a0f23ed7a6a436092e5f82d300f6965970
Daniel Vartanov <dan@vartanov.net>
2012-10-13 07:54:39 +0600

    Empty Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrackMyCourses::Application.config.secret_token = 'a52a48e9f365518967ed51b2968334904382f2e76c7e83d72073a1b614c3b3dde3a16055ab77bd4a156545db5f5a52d4f1c6c802aa08f2ae48485bb20103d457'
