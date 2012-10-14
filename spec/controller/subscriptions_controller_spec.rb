52ac37fc4ed406a243818783d51b1493ebc07085
Daniel Vartanov <dan@vartanov.net>
2012-10-14 09:40:55 +0600

    API endpoint for exposing a list of subscriptions

--- spec/controller/subscriptions_controller_spec.rb
+++ spec/controller/subscriptions_controller_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
 
 describe SubscriptionsController, :type => :controller do
-  describe "#create" do
-    context 'given various courses' do
-      let(:circuits) { Course.create! }
-      let(:chemistry) { Course.create! }
-      let(:pharmacy) { Course.create! }
-
+  context 'given various courses' do
+    let(:circuits) { Course.create! }
+    let(:chemistry) { Course.create! }
+    let(:pharmacy) { Course.create! }
+    
+    describe "#create" do
+      
       context 'anonymous user creates a subscrtipion' do
         before do
           post :create, :subscription => { :course_ids => [circuits.id] }
@@ -28,6 +29,36 @@ describe SubscriptionsController, :type => :controller do
 
           it "should update course list in the session" do
             session[:course_ids].should == [chemistry.id.to_s, pharmacy.id.to_s]
+          end
+        end
+      end
+    end
+    
+    describe "#index" do
+      context 'given anonymous user has created some subscriptions' do
+        before do
+          post :create, :subscription => { :course_ids => [chemistry.id, pharmacy.id] }
+        end
+
+        context 'when user gets a list of his subscriptions' do
+          before do
+            get :index
+          end
+
+          it 'should return a plain list of course ids' do
+            JSON.parse(response.body).should == [chemistry.id.to_s, pharmacy.id.to_s]
+          end
+        end
+      end
+
+      context 'given there is no subscriptions yet' do
+        context 'when user gets a list of his subscriptions' do
+          before do
+            get :index
+          end
+
+          it 'should return an empty array' do
+            JSON.parse(response.body).should == []
           end
         end
       end
