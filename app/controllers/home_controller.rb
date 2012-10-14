a51f15c54a737117d8db65225619b098629a6394
Kalys Osmonov <kalys@osmonov.com>
2012-10-15 00:38:16 +0600

    Fix redirect from home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     if logged_in?
-      redirect_to current_user.courses.any? ? dashboard_index_path : '/account/subscriptions'
+      redirect_to current_user.courses.any? ? dashboard_index_path(protocol: 'http') : '/account/subscriptions'
     end
   end
 end
